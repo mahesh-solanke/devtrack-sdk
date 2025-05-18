c18b2f25851a4f690d3760441d59ea0c3b1dc7a8
mahesh <maheshsolanke69@gmail.com>
2025-05-18 13:01:41 +0530

    Update DevTrackMiddleware to skip specific paths for tracking
    - Added a list of paths to skip tracking, including the backend URL and stats endpoint.
    - Simplified the dispatch method to check against the skip paths instead of a single URL.

--- devtrack_sdk/middleware/base.py
+++ devtrack_sdk/middleware/base.py
@@ -10,19 +10,17 @@ class DevTrackMiddleware(BaseHTTPMiddleware):
     stats = []
 
     def __init__(self, app, api_key: str, backend_url: str = "/__devtrack__/track"):
+        self.skip_paths = [backend_url, "/__devtrack__/stats"]
         self.api_key = api_key
         self.backend_url = backend_url
         super().__init__(app)
 
     async def dispatch(self, request: Request, call_next):
-        if request.url.path == self.backend_url:
+        if request.url.path in self.skip_paths:
             return await call_next(request)
 
-        response = await call_next(request)
-
+        start_time = datetime.now(timezone.utc)
         try:
-
-            start_time = datetime.now(timezone.utc)
             response = await call_next(request)
             log_data = extract_devtrack_log_data(request, response, start_time)
             DevTrackMiddleware.stats.append(log_data)
