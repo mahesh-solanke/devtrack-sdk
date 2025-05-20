674b97e64ef9943594b79a85b0ee3d36e48fc816
mahesh <maheshsolanke69@gmail.com>
2025-05-21 00:25:43 +0530

    🔖 Bump version to v0.2.0 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="devtrack-sdk",
-    version="0.1.2",
+    version="0.2.0",
     description="Middleware-based API analytics and observability tool for FastAPI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=["fastapi", "httpx", "starlette"],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
