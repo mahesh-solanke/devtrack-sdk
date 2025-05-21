7fd48f057bc98f43e13edc5afaafe8fe026ecc02
mahesh <maheshsolanke69@gmail.com>
2025-05-22 01:21:54 +0530

    🔖 Bump version to v0.2.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="devtrack-sdk",
-    version="0.2.1",
+    version="0.2.2",
     description="Middleware-based API analytics and observability tool for FastAPI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
